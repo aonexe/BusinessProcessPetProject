f48a08d9938dedf01e2409d69c519c1a94962f85
Vladislav Vorsin <aonexe@gmail.com>
2023-01-14 19:28:28 +0400

    Add User model Add spring-boot-starter-validation dependency

---
+++ src/main/java/models/User.java
@@ -0,0 +1,136 @@
+package models;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    @NotEmpty(message = "First name should be not empty")
+    @Size(min = 2, max = 30, message = "First name should be between 2 and 30 characters")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "Last name should be not empty")
+    @Size(min = 2, max = 30, message = "Last name should be between 2 and 30 characters")
+    private String lastName;
+
+
+    @Column(name = "date_of_birth")
+    @DateTimeFormat(pattern = "dd/MM/yyyy" )
+    private Date dateOfBirth;
+
+    @Column(name = "username")
+    @NotEmpty(message = "Username should be not empty")
+    @Size(min = 2, max = 30, message = "Username should be between 2 and 30 characters")
+    private String username;
+
+    @Column(name = "username")
+    @NotEmpty(message = "Password should be not empty")
+    @Size(min = 2, max = 30, message = "Password should be between 8 and 30 characters")
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_who")
+    @NotEmpty
+    private String createdWho;
+
+    public User() {}
+
+    public User(String firstName, String lastName, Date dateOfBirth, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getCreatedWho() {
+        return createdWho;
+    }
+
+    public void setCreatedWho(String createdWho) {
+        this.createdWho = createdWho;
+    }
+}
