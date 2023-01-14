f7c62565278bdf4d4e479b28ad2956f4d88d39dd
Vladislav Vorsin <aonexe@gmail.com>
2023-01-14 18:58:05 +0400

    Initial commit

---
+++ src/test/java/com/vorsin/businessProcess/BusinessProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vorsin.businessProcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
