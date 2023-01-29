92991487572aedb50dfd58640ad02d964cd87b48
Vladislav Vorsin <aonexe@gmail.com>
2023-01-30 00:35:21 +0400

    Refactor repo module

---
+++ src/main/java/com/vorsin/businessProcess/repositories/BPRepository.java
@@ -0,0 +1,15 @@
+package com.vorsin.businessProcess.repositories;
+
+import com.vorsin.businessProcess.models.BusinessProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BPRepository extends JpaRepository<BusinessProcess, Integer> {
+
+    Optional<BusinessProcess> findByTitle(String title);
+
+
+}
