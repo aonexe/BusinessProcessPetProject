af39944ed6716f8e280e503b25c1e8548f4a9881
aonexe <aonexe@gmail.com>
2023-02-01 20:39:31 +0400

    Add flyway migration
    - Clean up the code in BP

--- src/main/java/com/vorsin/businessProcess/repositories/UserRepository.java
+++ src/main/java/com/vorsin/businessProcess/repositories/UserRepository.java
@@ -3,11 +3,8 @@ package com.vorsin.businessProcess.repositories;
 import com.vorsin.businessProcess.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
-
 
    boolean existsByUsernameOrEmail(String username, String email);
 }
